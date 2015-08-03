10c6de6c893c75055dc91bc3cad1c9faf83328a8
Rafael Nowrotek <mail@benignware.com>
2015-08-03 17:47:23 +0200

    Select time instead of select date

--- lib/forecast/collection.rb
+++ lib/forecast/collection.rb
@@ -1,15 +1,15 @@
 class Forecast
   class Collection < Array
-    def select_date(date)
+    def select_time(time)
       result = nil
       date_forecasts = self.select do |obj|
-        obj.date.to_date == date.to_date
+        obj.time.to_date == time.to_date
       end
       if date_forecasts.length == 0
         return nil
       else
         hour_forecasts = date_forecasts.select do |obj|
-          obj.date.hour == obj.date.hour
+          obj.time.hour == obj.time.hour
         end
         if hour_forecasts.length > 0
           return hour_forecasts.first
@@ -17,10 +17,10 @@ class Forecast
         return date_forecasts.first
       end
       return nil
-      
     end
     
     private 
+      # Unused
       def seconds_between(date1, date2)
         ((Time.parse(date1.to_s) - Time.parse(date2.to_s)) / 3600).abs
       end
