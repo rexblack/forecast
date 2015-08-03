c97e89416cd5cf78c772b5ab22afa7c4bb6d9883
Rafael Nowrotek <mail@benignware.com>
2015-08-03 17:46:05 +0200

    Moved helper methods to utils

--- lib/forecast/adapter.rb
+++ lib/forecast/adapter.rb
@@ -19,21 +19,24 @@ class Forecast
     end
     
     def self.instance
+      options = {}
       provider = Forecast.config.provider
-      if provider.is_a?(Hash)
-        adapter_name = provider['adapter'].to_s
+      if provider.is_a?(String) || provider.is_a?(Symbol)
+        adapter_name = provider.to_sym
+      elsif provider.is_a?(Hash)
+        adapter_name = provider[:adapter].to_s
         options = provider.clone
         options.delete('adapter')
-      else
-        adapter_name = provider
-        options = {}
       end
       if adapter_name
+        if Forecast.config.adapters != nil && Forecast.config.adapters.has_key?(adapter_name)
+          options = options.merge(Forecast.config.adapters[adapter_name])
+        end
         adapter_classname = (adapter_name.to_s << "_adapter").split('_').collect!{ |w| w.capitalize }.join
         adapter_class = Object.const_get('Forecast').const_get("Adapters").const_get(adapter_classname)
         adapter_class.new(options)
       else 
-        puts 'no adapter provided'
+        puts 'Adapter not found'
       end
     end
     
@@ -51,89 +54,102 @@ class Forecast
     
     protected
     
-      def get_json(url, params = {})
-        if params.keys.count > 0
-          query_string = URI.encode_www_form(params)
-          url = url + "?" + query_string
-        end
-        resp = Net::HTTP.get_response(URI.parse(url))
-        data = resp.body
-        result = JSON.parse(data)
-        if result && result['cod'] != "404"
-          return result
-        end
-        return nil
-      end
-      
-      def get_doc(url, params = {})
-        if params.keys.count > 0
-          query_string = URI.encode_www_form(params)
-          url = url + "?" + query_string
-        end
-        xml_data = Net::HTTP.get_response(URI.parse(url)).body
-        doc = REXML::Document.new(xml_data)
-        return doc
-      end
-      
-      def metric(fahrenheit)
-        ((fahrenheit - 32) / 1.800).round()
-      end
-      
-      def get_temp(fahrenheit)
-        fahrenheit = fahrenheit.to_i.round()
-        Forecast.config.temp_scale.to_sym == :metric ? metric(fahrenheit) : fahrenheit
-      end
-      
-      def similar_words(first, second)
-        similar_words = 0.0
-        first_words = first.downcase.split(/\s+/)
-        second_words = second.downcase.split(/\s+/)
-        first_words.each do |first_word|
-          second_words.each do |second_word|
-            similar = 0.0
-            if first_word == second_word
-              similar = 1.0
-            else
-              l1 = levenshtein(first_word, second_word)
-              if l1 > 0 && l1 < 3
-                similar = 1.0
-              end
-            end
-            similar_words+= similar
+      def get_temperature(value, input = :fahrenheit )
+        if value == nil
+          value = 0
+        elsif value.is_a?(Array)
+          value = value.inject{ |sum, v| sum.to_f + get_temperature(v) }.to_f / value.size
+        elsif value.is_a?(String) || value.is_a?(Numeric)
+          value = value.to_f
+          if input == :fahrenheit && Forecast.config.scale.to_sym == :kelvin
+            value = Forecast::Utils.fahrenheit_to_kelvin(value)
+          elsif input == :fahrenheit && Forecast.config.scale.to_sym == :celsius
+            value = Forecast::Utils.fahrenheit_to_celsius(value)
+          elsif input == :kelvin && Forecast.config.scale.to_sym == :fahrenheit
+            value = Forecast::Utils.kelvin_to_fahrenheit(value)
+          elsif input == :kelvin && Forecast.config.scale.to_sym == :celsius
+            value = Forecast::Utils.kelvin_to_celsius(value)
+          elsif input == :celsius && Forecast.config.scale.to_sym == :fahrenheit
+            value = Forecast::Utils.celsius_to_fahrenheit(value)
+          elsif input == :celsius && Forecast.config.scale.to_sym == :kelvin
+            value = Forecast::Utils.celsius_to_kelvin(value)
           end
         end
-        count = first_words.concat(second_words).uniq.length
-        similarity = similar_words / count
-        return similarity
+        value.round
       end
       
-      def levenshtein(first, second)
-        matrix = [(0..first.length).to_a]
-        (1..second.length).each do |j|
-          matrix << [j] + [0] * (first.length)
+      def get_time(value)
+        if value.is_a?(Time) || value.is_a?(Date)
+          value.to_datetime
+        elsif value.is_a?(String) && value =~ /\A\d+\Z/ || value.is_a?(Numeric)
+          Time.at(value.to_i).to_datetime
+        elsif value.is_a?(String)
+          DateTime.parse(value.to_s)
+        elsif value.is_a?(DateTime)
+          value
         end
-        (1..second.length).each do |i|
-          (1..first.length).each do |j|
-            if first[j-1] == second[i-1]
-              matrix[i][j] = matrix[i-1][j-1]
-            else
-              matrix[i][j] = [
-                matrix[i-1][j],
-                matrix[i][j-1],
-                matrix[i-1][j-1],
-              ].min + 1
-            end
-          end
-        end
-        return matrix.last.last
       end
       
-      def get_condition_by_similarity(name)
+      def get_text(value)
+        value.capitalize
+      end
+      
+      def get_condition_synonyms(name)
+        result = []
+        synonyms = Forecast.config.synonyms
+        synonym_values = synonyms.flatten.select { |v|
+          Forecast::Utils.word_similarity(name, v) > 0.6
+        }
+        if synonym_values.size > 0
+          c = synonym_values.sort { |a, b| 
+            as = Forecast::Utils.word_similarity(name, a)
+            bs = Forecast::Utils.word_similarity(name, b)
+            as <=> bs
+          }.reverse
+          match = c.first
+          result = synonyms.select do |v|
+           v.include?(match)
+          end.first
+        end
+        return result
+      end
+      
+      
+      def get_similar_condition(name)
         conditions = Forecast.config.conditions
-          c = conditions.values.sort { |a, b| similar_words(name, a) <=> similar_words(name, b) }.reverse
-          if c.first && similar_words(name, c.first) > 0
-            return c.first
+        condition_synonyms = {}
+        conditions.each do |k, v|
+          condition_synonyms[v] = get_condition_synonyms(v) - [v]
+        end
+        condition_synonym_similarity = {}
+        conditions.each do |k, v|
+          synonyms = [v] + get_condition_synonyms(v)
+          condition_synonym_similarity[v] = get_condition_synonyms(v) - [v]
+          similarity = 0
+          synonyms.each do |synonym|
+            similarity = [similarity, Forecast::Utils.word_similarity(name, synonym)].max
+            #if name == 'Thundershowers'
+              #puts "simlarity #{name} => #{synonym} : #{similarity.to_s}"
+            #end
           end
+          condition_synonym_similarity[v] = similarity
+        end
+        c = conditions.values.select { |condition|
+          condition_synonym_similarity[condition] > 0.1
+        }
+        c = c.sort { |a, b|
+          a_similarity = condition_synonym_similarity[a]
+          b_similarity = condition_synonym_similarity[b]
+          #puts 'cmp: ' + name + " =>  #{a} = #{b} ----> " + a_similarity.to_s + " === " + b_similarity.to_s + ""
+          a_similarity <=> b_similarity
+        }.reverse
+        #puts 'F: ' + c.first.to_s
+        if c.first != nil
+          return {
+            condition: c.first,
+            similarity: condition_synonym_similarity[c.first] || 0
+          }
+        end
       end
       
       def get_condition_name(match)
@@ -141,7 +157,7 @@ class Forecast
           return nil
         end
         conditions = Forecast.config.conditions
-        condition = "Unknown"
+        condition = nil
         if conditions.keys.include?(match)
           condition = conditions[match]
         elsif conditions.values.include?(match)
@@ -150,55 +166,35 @@ class Forecast
         return condition
       end
       
-      def match_adapter_condition(api_condition)
-        match = nil
-        conditions = config['conditions']
-        if conditions != nil
-          conditions.each do |key, value|
-            # puts "match key #{api_condition} -> #{key.to_s}, #{value.to_s}"
-            if is_numeric?(api_condition) && key.is_a?(String)
-              if key.include? ".."
-                range = key.split(/\.{2}/)
-                if api_condition.to_i >= range[0].to_i && api_condition.to_i <= range[1].to_i
-                  match = value
-                end
-              end
-            elsif key.to_s == api_condition.to_s
-              match = value;
-            end
-          end
-        end
-        return match
-      end
-      
       def get_condition(api_conditions)
         if !api_conditions.is_a?(Array)
           api_conditions = [api_conditions]
         end
         condition = nil
-        api_conditions.each do |api_condition|
-          match = match_adapter_condition(api_condition)
-          if condition == nil
-            condition = get_condition_by_similarity(api_condition)
+        if api_conditions.length > 0
+          similar_conditions = api_conditions.map { |api_condition|
+            get_similar_condition(api_condition)
+          }.select {|v|
+            v != nil  
+          }
+          #puts similar_conditions.to_s
+          similar_condition = similar_conditions.sort { |a,b|
+            a[:similarity] <=> b[:similarity]
+          }.reverse.first
+          if similar_condition
+            condition = similar_condition[:condition]
+            condition_name = get_condition_name(condition)
+            if condition_name == nil
+              condition_name = api_conditions[0]
+            end
           end
-          if condition
-            break
+          if condition_name != nil
+            return condition_name
           end
         end
-        get_condition_name(condition)
+        return "Unknown"
       end
       
-      private 
-      
-        def is_numeric?(s)
-          begin
-            Float(s)
-          rescue
-            false # not numeric
-          else
-            true # numeric
-          end
-        end
       
   end
 end
